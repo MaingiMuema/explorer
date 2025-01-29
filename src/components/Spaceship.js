a5f0997e0fc25aea4c9fa9ec4ac674ba3c50361a
Samuel Ndolo <markmaingi@kabarak.ac.ke>
2025-01-29 18:47:25 +0300

    'changes'

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -15,130 +15,125 @@ const Spaceship = (props) => {
 
   return (
     <group ref={group} {...props}>
-      {/* Main body - now more streamlined */}
+      {/* Main body */}
       <mesh position={[0, 0, 0]}>
         <cylinderGeometry args={[0.7, 1.0, 4.5, 12]} />
-        <meshStandardMaterial color="#2c3e50" metalness={0.9} roughness={0.1} />
+        <meshStandardMaterial color="#e8e8e8" metalness={0.9} roughness={0.1} />
       </mesh>
 
-      {/* Front nose cone - sharper and more angular */}
+      {/* Front nose cone */}
       <mesh position={[0, 0, 2.5]}>
         <coneGeometry args={[0.7, 2.5, 12]} />
-        <meshStandardMaterial color="#34495e" metalness={0.9} roughness={0.1} />
+        <meshStandardMaterial color="#ffffff" metalness={0.9} roughness={0.1} />
       </mesh>
 
-      {/* Enhanced wings with accent lighting */}
+      {/* Wings */}
       <group>
-        {/* Left wing */}
+        {/* Left & Right wings - base color */}
         <mesh position={[-1.8, 0, 0]} rotation={[0, 0.2, Math.PI * 0.12]}>
           <boxGeometry args={[3.5, 0.08, 2.5]} />
           <meshStandardMaterial
-            color="#34495e"
+            color="#f0f0f0"
             metalness={0.9}
             roughness={0.1}
           />
         </mesh>
-
-        {/* Wing accent lights - left */}
-        <mesh position={[-2.2, 0.1, 0]}>
-          <boxGeometry args={[2.8, 0.02, 0.05]} />
-          <meshStandardMaterial
-            color="#3498db"
-            emissive="#3498db"
-            emissiveIntensity={2}
-            toneMapped={false}
-          />
-        </mesh>
-
-        {/* Right wing */}
         <mesh position={[1.8, 0, 0]} rotation={[0, -0.2, -Math.PI * 0.12]}>
           <boxGeometry args={[3.5, 0.08, 2.5]} />
           <meshStandardMaterial
-            color="#34495e"
+            color="#f0f0f0"
             metalness={0.9}
             roughness={0.1}
           />
         </mesh>
 
-        {/* Wing accent lights - right */}
+        {/* Wing accent lights */}
+        <mesh position={[-2.2, 0.1, 0]}>
+          <boxGeometry args={[2.8, 0.02, 0.05]} />
+          <meshStandardMaterial
+            color="#00ffff"
+            emissive="#00ffff"
+            emissiveIntensity={3}
+            toneMapped={false}
+          />
+        </mesh>
         <mesh position={[2.2, 0.1, 0]}>
           <boxGeometry args={[2.8, 0.02, 0.05]} />
           <meshStandardMaterial
-            color="#3498db"
-            emissive="#3498db"
-            emissiveIntensity={2}
+            color="#00ffff"
+            emissive="#00ffff"
+            emissiveIntensity={3}
             toneMapped={false}
           />
         </mesh>
       </group>
 
-      {/* Enhanced engine section */}
+      {/* Engine section */}
       <group position={[0, 0, -2.2]}>
-        {/* Main engine housing */}
         <mesh>
           <cylinderGeometry args={[0.8, 0.9, 0.8, 12]} />
           <meshStandardMaterial
-            color="#2c3e50"
+            color="#e8e8e8"
             metalness={0.9}
             roughness={0.1}
           />
         </mesh>
 
-        {/* Engine glow effects */}
+        {/* Engine glow */}
         {[0.2, 0.1, 0].map((z, i) => (
           <mesh key={i} position={[0, 0, -z]}>
             <cylinderGeometry args={[0.5 - i * 0.1, 0.5 - i * 0.1, 0.1, 12]} />
             <meshStandardMaterial
-              color="#3498db"
-              emissive="#3498db"
-              emissiveIntensity={3 - i}
+              color="#00ffff"
+              emissive="#00ffff"
+              emissiveIntensity={4 - i}
               toneMapped={false}
               transparent={true}
-              opacity={0.8}
+              opacity={0.9}
             />
           </mesh>
         ))}
       </group>
 
-      {/* Enhanced cockpit with better transparency */}
+      {/* Cockpit */}
       <mesh position={[0, 0.6, 1.2]}>
         <sphereGeometry
           args={[0.5, 24, 24, 0, Math.PI * 2, 0, Math.PI * 0.6]}
         />
         <meshPhysicalMaterial
-          color="#81ecec"
+          color="#80ffff"
           metalness={0.2}
           roughness={0.1}
           transmission={0.9}
           thickness={0.5}
-          opacity={0.7}
+          opacity={0.8}
           transparent={true}
         />
       </mesh>
 
-      {/* Additional surface details */}
+      {/* Surface details */}
       <group>
-        {/* Enhanced panel lines */}
+        {/* Panel lines */}
         {[-0.6, -0.2, 0.2, 0.6].map((x, i) => (
           <mesh key={i} position={[x, 0.3, 0]}>
             <boxGeometry args={[0.03, 0.01, 3]} />
             <meshStandardMaterial
-              color="#74b9ff"
-              emissive="#74b9ff"
-              emissiveIntensity={0.5}
+              color="#00ffff"
+              emissive="#00ffff"
+              emissiveIntensity={2}
               toneMapped={false}
             />
           </mesh>
         ))}
 
-        {/* Decorative tech panels */}
+        {/* Tech panels */}
         {[-1, 1].map((x, i) => (
           <mesh key={i} position={[x * 0.4, 0.4, 0]}>
             <boxGeometry args={[0.3, 0.2, 0.05]} />
             <meshStandardMaterial
-              color="#0984e3"
-              emissive="#0984e3"
-              emissiveIntensity={0.5}
+              color="#40ffff"
+              emissive="#40ffff"
+              emissiveIntensity={1.5}
               toneMapped={false}
             />
           </mesh>
